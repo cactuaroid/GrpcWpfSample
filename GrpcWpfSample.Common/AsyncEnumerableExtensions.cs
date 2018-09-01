863d72691decf5ada6e384d9d516f18bb903b2b3
cactuaroid <solamiredoredore@gmail.com>
2018-09-01 15:15:45 +0900

    docs: Add document comment on AsyncEnumerableExtensions

--- GrpcWpfSample.Common/AsyncEnumerableExtensions.cs
+++ GrpcWpfSample.Common/AsyncEnumerableExtensions.cs
@@ -8,7 +8,16 @@ namespace GrpcWpfSample.Common
 {
     public static class AsyncEnumerableExtensions
     {
-        // refer https://github.com/dotnet/reactive/issues/144
+        /// <summary>
+        /// ForEachAsync for async lambda.
+        /// </summary>
+        /// <remarks>
+        /// https://github.com/dotnet/reactive/issues/144
+        /// </remarks>
+        /// <typeparam name="TSource">source type</typeparam>
+        /// <param name="source">source sequence</param>
+        /// <param name="funcTask">foreach body</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         public static async Task ForEachTaskAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, Task> funcTask)
         {
             using (var e = source.GetEnumerator())
@@ -20,7 +29,17 @@ namespace GrpcWpfSample.Common
             }
         }
 
-        // from https://github.com/maca88/AsyncGenerator/issues/94#issuecomment-385286972
+        /// <summary>
+        /// Projects each element of a sequence into a new form of IAsyncEnumerable.
+        /// </summary>
+        /// <remarks>
+        /// https://github.com/maca88/AsyncGenerator/issues/94#issuecomment-385286972
+        /// </remarks>
+        /// <typeparam name="T">source type</typeparam>
+        /// <typeparam name="TResult">result type</typeparam>
+        /// <param name="enumerable">A sequence of values to invoke a transform function on</param>
+        /// <param name="selector">A transform function to apply to each element</param>
+        /// <returns>An IAsyncEnumerable whose elements are the result of invoking the transform function on each element of source.</returns>
         public static IAsyncEnumerable<TResult> SelectAsync<T, TResult>(this IEnumerable<T> enumerable, Func<T, Task<TResult>> selector)
         {
             return AsyncEnumerable.CreateEnumerable(() =>
